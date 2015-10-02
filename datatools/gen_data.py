3a9e2f5f7ce4530644ef15fb89c367c97e3669a4
Shannon Fenn <shannon.fenn@gmail.com>
2015-10-02 15:48:29 +1000

    cleanup

--- datatools/gen_data.py
+++ datatools/gen_data.py
@@ -14,7 +14,8 @@ FUNCTIONS = {
 
 def to_binary(value, num_bits):
     # little-endian
-    return np.flipud(np.array([int(i) for i in np.binary_repr(value, num_bits)]))
+    return np.flipud(np.array(
+        [int(i) for i in np.binary_repr(value, num_bits)]))
 
 
 def two_input_mapping(num_bits_per_operand, functor):
@@ -57,14 +58,17 @@ def main(args):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Generate json file for given function.')
+    parser = argparse.ArgumentParser(
+        description='Generate json file for given function.')
     parser.add_argument('numbits', type=int,
                         help='number of input bits per operand')
-    parser.add_argument('function', metavar='function', choices=FUNCTIONS.keys(),
-                        help='function to generate data for, one of: {}.'.format(
-                            list(FUNCTIONS.keys())))
-    parser.add_argument('--outfile', type=str, help=('output filename - if omitted will default '
-                        'to functions/[function][n].npz'))
+    parser.add_argument('function', metavar='function',
+                        choices=FUNCTIONS.keys(),
+                        help='function to generate data for, one of: {}.'.
+                        format(list(FUNCTIONS.keys())))
+    parser.add_argument('--outfile', type=str,
+                        help=('output filename - if omitted will default '
+                              'to functions/[function][n].npz'))
     parser.add_argument('--outlim', metavar='k', type=int,
                         help=('(optional) number of bits to limit output to, '
                               'takes the k least significant.'))
