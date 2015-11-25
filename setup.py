7ba2a816e09a74d1425bb08cec8b75ff092aac6b
Shannon Fenn <shannonfenn@gmail.com>
2015-11-25 23:48:44 +1100

    distutils setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='datatools',
+    version='0.1.0',
+    author='S. Fenn',
+    author_email='shannon.fenn@gmail.com',
+    packages=['datatools'],
+    scripts=[],
+    url='https://github.com/shannonfenn/data-tools',
+    license='LICENSE.txt',
+    description='Tools for data generation and handling.',
+    long_description='',
+    install_requires=[
+        "numpy >= 1.10.1",
+        "pandas >= 0.17.0",
+    ],
+)
