0999683abe5b4261f72f10bc6978f38be00a7a88
Shannon Fenn <shannon.fenn@gmail.com>
2015-11-26 16:29:22 +1100

    unit tests for gen_samples_dichotomous mostly finished

--- datatools/test/test_gen_samples_dichotomous.py
+++ datatools/test/test_gen_samples_dichotomous.py
@@ -1,13 +1,10 @@
-from datatools.gen_samples_dichotomous import unique_valid_samples
-from datatools.gen_samples_dichotomous import single_unique_valid_sample
-from datatools.gen_samples_dichotomous import OPERATOR_MAP, operator_function
+import datatools.gen_samples_dichotomous as gen
 import numpy as np
 import pytest
+import os
 
-
-@pytest.fixture(params=OPERATOR_MAP.keys())
-def operator_name(request):
-    return request.param
+DATA_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                        'target_matrices')
 
 
 @pytest.mark.parametrize("opname,Ni,No,Ne", [
@@ -16,19 +13,25 @@ def operator_name(request):
     ('sub', 4, 2, 16),
     ('add', 8, 3, 4),
     ('mul', 8, 3, 64),
-    ('div', 8, 4, 128),
+    ('and', 8, 4, 128),
 ])
 def test_single_unique_valid_sample(opname, Ni, No, Ne):
-    samples = np.zeros(Ne, dtype=np.uint64)
-    op = operator_function(opname, Ni, No)
-    single_unique_valid_sample(op, Ni, No, Ne, samples)
+    sample = np.zeros(Ne, dtype=np.uint64)
+    op = gen.operator_function(opname, Ni, No)
+    gen.single_unique_valid_sample(op, Ni, No, Ne, sample)
 
-    # need to generate output patterns from samples
+    # generate output patterns from sample
+    filename = os.path.join(DATA_DIR, '{}{}.npy'.format(opname, Ni))
+    full_target = np.load(filename)
+    target_matrix = full_target[sample, :]
 
-    assert np.any(samples)
-    assert not np.all(samples)
+    print(target_matrix)
+
+    assert np.all(np.any(target_matrix, axis=0))
+    assert not np.any(np.all(target_matrix, axis=0))
 
 
+@pytest.mark.xfail
 @pytest.mark.parametrize("opname,Ni,No,Ne,Ns", [
     ('add', 4, 1, 8, 50),
     ('sub', 4, 2, 12, 50),
@@ -38,7 +41,13 @@ def test_single_unique_valid_sample(opname, Ni, No, Ne):
     ('div', 8, 8, 128, 150),
 ])
 def test_unique_valid_samples(opname, Ni, No, Ne, Ns):
-    op = operator_function(opname, Ni, No)
-    samples = unique_valid_samples(op, Ni, No, Ns, Ne)
-    assert np.all(np.any(samples, axis=1))
-    assert not np.any(np.all(samples, axis=1))
+    op = gen.operator_function(opname, Ni, No)
+    samples = gen.unique_valid_samples(op, Ni, No, Ns, Ne)
+
+    # generate output patterns from sample
+    filename = os.path.join(DATA_DIR, '{}{}.npy'.format(opname, Ni))
+    full_target = np.load(filename)
+    target_matrix = full_target[samples, :]
+
+    assert np.all(np.any(target_matrix, axis=2))
+    assert not np.any(np.all(target_matrix, axis=2))
