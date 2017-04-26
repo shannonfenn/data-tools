ab31fcb31dbd4ec481ab9350e9b545fe55b9d1ad
Shannon Fenn <shannon.fenn@gmail.com>
2017-04-26 12:33:54 +1000

    [feature] minfs dependency graphs

--- datatools/drawing.py
+++ datatools/drawing.py
@@ -1,6 +1,9 @@
 import palettable
+import numpy as np
 import networkx as nx
+from networkx.drawing.nx_agraph import graphviz_layout, write_dot
 import datatools.network as nt
+import datatools.utils as utils
 from itertools import cycle
 
 
@@ -73,3 +76,45 @@ def draw_bn(G, ax, show_dangling=True, node_size=400, reposition=False):
     nx.draw_networkx_labels(G, pos, labels, ax=ax, font_size=16)
 
     ax.axis('off')
+
+
+def fs_dependency_graph(F, R, a_thresh=0, r_thresh=0.0):
+    G = nx.DiGraph()
+
+    for i, f in enumerate(F):
+        G.add_node(i, fs=f)
+
+    ranks, inverse = np.unique(R, return_inverse=True)
+    for i in range(1, len(ranks)):
+        for i0 in range(0, i):
+            locs0 = np.where(inverse == i0)[0]
+            locs1 = np.where(inverse == i)[0]
+
+            for l0 in locs0:
+                for l1 in locs1:
+                    a_overlap = len(set(F[l0]).intersection(F[l1]))
+                    r_overlap = utils.overlap(F[l0], F[l1])
+                    if (a_overlap > a_thresh and r_overlap > r_thresh):
+                        G.add_edge(l0, l1, a=a_overlap, r=r_overlap)
+
+    return G
+
+
+def draw_dependency_graph(F, R=None, a_thresh=0, r_thresh=0.0,
+                          path=None, **nxargs):
+    if R is None:
+        R = [len(f) for f in F]
+
+    G = fs_dependency_graph(F, R, a_thresh, r_thresh)
+
+    pos = graphviz_layout(G, 'dot')
+    weights = nx.get_edge_attributes(G, 'a')
+    nodelabels = {n: '{} [{}]'.format(n, len(d['fs']))
+                  for n, d in G.nodes_iter(data=True)}
+    if path is None:
+        nx.draw(G, pos, labels=nodelabels, edge_labels=weights, **nxargs)
+        nx.draw_networkx_edge_labels(G, pos, edge_labels=weights)
+    else:
+        nx.set_edge_attributes(G, 'label', weights)
+        G = nx.relabel_nodes(G, nodelabels)
+        write_dot(G, path)
