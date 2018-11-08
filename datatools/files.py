02c8e8c909096b982e2f195b4c3fcc61fde07ec1
Shannon Fenn <shannon.fenn@gmail.com>
2018-11-08 15:46:24 +0900

    reorganised file and plotting utils

--- datatools/files.py
+++ datatools/files.py
@@ -2,6 +2,86 @@ import glob
 import os.path
 import feather
 import pandas as pd
+from natsort import natsorted
+
+
+# Boolean Network Constants
+BN_TREATMENTS = ['strat-raps-reduced', 'mono-e6-auto', 'split-cplex', 'cc-auto']
+BN_TREATMENT_MAP = {
+    # 'strat-raps-reduced':  'IFF',
+    # 'mono-e1':             'Monolithic ($L_1$)',
+    # 'mono-e6-auto':        'Monolithic ($L_{gh}$)',
+    # 'split-cplex':         'IC (w/ feature selection)',
+    # 'strat-cplex-reduced': 'Stratified / Optimal',
+    # 'cc-auto':             'CC (w/ curriculum)',
+    # 'cc-random':           'CC (random)',
+    'strat-raps-reduced':  'IFF',
+    'mono-e6-auto':        'Monolithic',
+    'split-cplex':         'IC',
+    'cc-auto':             'CC',
+}
+
+BN_NON_ORDERED_TREATMENTS = [
+    BN_TREATMENT_MAP[k]
+    for k in ['mono-e1', 'split-cplex', 'split-raps', 'cc-random']
+    if k in BN_TREATMENT_MAP]
+
+BN_APRIORI_TREATMENTS = [
+    BN_TREATMENT_MAP[k]
+    for k in ['mono-e2-auto', 'mono-e3-auto', 'mono-e6-auto', 'cc-auto']
+    if k in BN_TREATMENT_MAP]
+
+# Neural Network Constants
+NN_TREATMENTS = ['layered', 'parallel', 'shared_a', 'shared_b', 'shared_c']
+NN_TREATMENT_MAP = {
+    'layered':  'IFF',
+    'parallel': 'IC',
+    'shared_a': 'Wide',
+    'shared_b': 'Deep',
+    'shared_c': 'Dovetailed',
+}
+
+NN_NON_ORDERED_TREATMENTS = [
+    NN_TREATMENT_MAP[k]
+    for k in ['parallel', 'shared_a', 'shared_b']
+    if k in NN_TREATMENT_MAP]
+
+NN_APRIORI_TREATMENTS = [
+    NN_TREATMENT_MAP[k]
+    for k in ['shared_c']
+    if k in NN_TREATMENT_MAP]
+A_PRIORI_TMT_NAME = 'a priori'
+
+DATASET_NAME_MAP = {
+    'cmaj9': '9-bit cascaded majority',
+    'cmux15': '15-bit cascaded multiplexer',
+    'cpar7': '7-bit cascaded parity',
+    }
+for i in [3, 4, 5, 6, 7, 8, 16, 32, 48, 64, 96, 128]:
+    DATASET_NAME_MAP[f'add{i}'] = f'{i}-bit'
+for i, c in zip(range(10), 'ABCDEFGHIJ'):
+    DATASET_NAME_MAP[f'gen{i}'] = f'{c}'
+
+
+def update_names(df, treatment_map=BN_TREATMENT_MAP, treatments=BN_TREATMENTS):
+    treatments = df.treatment.cat.categories.intersection(treatments)
+    df.dataset.cat.rename_categories(DATASET_NAME_MAP, inplace=True)
+    df.dataset.cat.reorder_categories(natsorted(df.dataset.cat.categories),
+                                      ordered=True, inplace=True)
+    df = df[df.treatment.isin(treatments)]
+    df.treatment.cat.rename_categories(treatment_map, inplace=True)
+    df.treatment.cat.remove_unused_categories(inplace=True)
+    df.treatment.cat.reorder_categories([treatment_map[k] for k in treatments],
+                                        ordered=True, inplace=True)
+    return df
+
+
+def convert_treatment_to_curricula(df, non_ordered, apriori):
+    df = df[~(df.treatment.isin(non_ordered))].copy()
+    df.treatment.cat.add_categories(['a priori'], inplace=True)
+    df.treatment[df.treatment.isin(apriori)] = A_PRIORI_TMT_NAME
+    df.treatment.cat.remove_unused_categories(inplace=True)
+    return df
 
 
 def load_results(directory, remove_failed=False):
--- datatools/plotting/utils.py
+++ datatools/plotting/utils.py
@@ -4,55 +4,8 @@ import functools
 import seaborn as sns
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-from natsort import natsorted
 import datatools.measures as measures
 
-
-# Boolean Network Constants
-BN_TREATMENTS = ['strat-raps-reduced', 'mono-e6-auto', 'split-cplex', 'cc-auto']
-BN_TREATMENT_MAP = {
-    # 'strat-raps-reduced':  'IFF',
-    # 'mono-e1':             'Monolithic ($L_1$)',
-    # 'mono-e6-auto':        'Monolithic ($L_{gh}$)',
-    # 'split-cplex':         'IC (w/ feature selection)',
-    # 'strat-cplex-reduced': 'Stratified / Optimal',
-    # 'cc-auto':             'CC (w/ curriculum)',
-    # 'cc-random':           'CC (random)',
-    'strat-raps-reduced':  'IFF',
-    'mono-e6-auto':        'Monolithic',
-    'split-cplex':         'IC',
-    'cc-auto':             'CC',
-}
-
-BN_NON_ORDERED_TREATMENTS = [
-    BN_TREATMENT_MAP[k]
-    for k in ['mono-e1', 'split-cplex', 'split-raps', 'cc-random']
-    if k in BN_TREATMENT_MAP]
-
-BN_APRIORI_TREATMENTS = [
-    BN_TREATMENT_MAP[k]
-    for k in ['mono-e2-auto', 'mono-e3-auto', 'mono-e6-auto', 'cc-auto']
-    if k in BN_TREATMENT_MAP]
-
-# Neural Network Constants
-NN_TREATMENTS = ['layered', 'parallel', 'shared_a', 'shared_b', 'shared_c']
-NN_TREATMENT_MAP = {
-    'layered':  'IFF',
-    'parallel': 'IC',
-    'shared_a': 'Wide',
-    'shared_b': 'Deep',
-    'shared_c': 'Dovetailed',
-}
-
-NN_NON_ORDERED_TREATMENTS = [
-    NN_TREATMENT_MAP[k]
-    for k in ['parallel', 'shared_a', 'shared_b']
-    if k in NN_TREATMENT_MAP]
-
-NN_APRIORI_TREATMENTS = [
-    NN_TREATMENT_MAP[k]
-    for k in ['shared_c']
-    if k in NN_TREATMENT_MAP]
 
 # Plot elements
 TEST_MCC_LABEL = 'Test MCC'
@@ -63,22 +16,11 @@ TAU_LABEL = r'Cumulative Rank Correlation ($\tau$)'
 TARGET_LABEL = 'Target'
 NE_LABEL = 'Training Set Size'
 GEN_FREQ_LABEL = 'Generalisation Frequency'
-A_PRIORI_LABEL = 'a priori'
 METRIC_LABELS = {
     'test_mcc': TEST_MACRO_MCC_LABEL,
     'time_learning': TRG_TIME_LABEL,
     'tau': TAU_LABEL,
 }
-
-DATASET_NAME_MAP = {
-    'cmaj9': '9-bit cascaded majority',
-    'cmux15': '15-bit cascaded multiplexer',
-    'cpar7': '7-bit cascaded parity',
-    }
-for i in [3, 4, 5, 6, 7, 8, 16, 32, 48, 64, 96, 128]:
-    DATASET_NAME_MAP[f'add{i}'] = f'{i}-bit'
-for i, c in zip(range(10), 'ABCDEFGHIJ'):
-    DATASET_NAME_MAP[f'gen{i}'] = f'{c}'
 
 
 def set_common_params():
@@ -96,27 +38,6 @@ def set_common_params():
     mpl.rcParams['figure.dpi'] = 600
     mpl.rcParams['savefig.dpi'] = 600
     mpl.rcParams['legend.frameon'] = False
-
-
-def update_names(df, treatment_map=BN_TREATMENT_MAP, treatments=BN_TREATMENTS):
-    treatments = df.treatment.cat.categories.intersection(treatments)
-    df.dataset.cat.rename_categories(DATASET_NAME_MAP, inplace=True)
-    df.dataset.cat.reorder_categories(natsorted(df.dataset.cat.categories),
-                                      ordered=True, inplace=True)
-    df = df[df.treatment.isin(treatments)]
-    df.treatment.cat.rename_categories(treatment_map, inplace=True)
-    df.treatment.cat.remove_unused_categories(inplace=True)
-    df.treatment.cat.reorder_categories([treatment_map[k] for k in treatments],
-                                        ordered=True, inplace=True)
-    return df
-
-
-def convert_treatment_to_curricula(df, non_ordered, apriori):
-    df = df[~(df.treatment.isin(non_ordered))].copy()
-    df.treatment.cat.add_categories(['a priori'], inplace=True)
-    df.treatment[df.treatment.isin(apriori)] = A_PRIORI_LABEL
-    df.treatment.cat.remove_unused_categories(inplace=True)
-    return df
 
 
 def plt_mcc_time_vs_ne_all_datasets(df, sharex=False, sharey=False):
@@ -169,10 +90,7 @@ def plt_cum_mcc(df, normalise=False, errors=True, ax=None, plot_kwargs={}):
     return ax
 
 
-def plt_cum_tau(df, normalise=False, errors=True,
-                non_ordered=BN_NON_ORDERED_TREATMENTS,
-                apriori=BN_APRIORI_TREATMENTS, ax=None, plot_kwargs={}):
-    df = convert_treatment_to_curricula(df, non_ordered, apriori)
+def plt_cum_tau(df, normalise=False, errors=True, ax=None, plot_kwargs={}):
     ax = _plot_cumulative_vs_dataset(df, 'tau', normalise, errors,
                                      legend_title='Curriculum', ax=ax,
                                      plot_kwargs=plot_kwargs)
